c40dabe37b58b6fb0599025a25f93f5597fb0db2
SAMURAY-A <abdullohaziz559@gmail.com>
2024-12-07 19:30:43 +0500

    hi

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,6 +3,7 @@ import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 export default function TodoItem({ title, deleteItem, id, editItem }) {
     return (
+        
         <div>
             <Card>
                 <CardHeader>
